5344f67f8371ac559f982ab2b52ca9c09b1b26c1
Eduardo Viveros <111369460+Eduardo0198@users.noreply.github.com>
2022-12-02 11:55:28 -0600

    Add files via upload

---
+++ Fecha.h
@@ -0,0 +1,25 @@
+#ifndef FECHA_H
+#define FECHA_H
+//Archivo .h de nuestra clase Fecha aqui declaramos nuestros atributes y metodos
+
+
+class Fecha{
+	private:
+		int dia;
+		int mes;
+
+	public:
+		Fecha();
+		~Fecha();
+		Fecha(int, int);
+		//Getters
+		int getDia();
+		int getMes();
+		
+		//Setters
+		void setDia(int);
+		void setMes(int);
+		void calendario();	
+};
+
+#endif

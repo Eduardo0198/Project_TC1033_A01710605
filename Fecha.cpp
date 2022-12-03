a6d6d7c9a2b573ce5fe2d95f4e598d7002a32a5a
Eduardo Viveros <111369460+Eduardo0198@users.noreply.github.com>
2022-12-02 23:15:20 -0600

    Add files via upload

---
+++ Fecha.cpp
@@ -0,0 +1,40 @@
+
+//Archivo Fecha
+
+#include "Fecha.h"
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+//Archivo .cpp de nuestra clase Fecha aqui declaramos nuestros getters y setters
+
+
+using namespace std;
+//CONSTRUCTOR SIN PARAMETROS SOLO INICIALIZAMOS LOS VALORES
+Fecha::Fecha(){
+	dia = 3;
+	mes = 10;
+};
+
+Fecha::~Fecha(){};
+
+//Constrcutor con parametros
+Fecha::Fecha(int dia_, int mes_){
+	dia = dia_;
+	mes = mes_;
+}
+int Fecha::getMes(){
+	return mes;
+}
+int Fecha::getDia(){
+	return dia;
+}
+void Fecha::setMes(int mes_){
+	mes = mes_;
+}
+void Fecha::setDia(int dia_){
+	dia = dia_;
+}
+void Fecha::calendario(){
+	cout<<"\t\tHoy "<<dia<<" de "<<mes<<" del 2022"<<endl;
+}
+
